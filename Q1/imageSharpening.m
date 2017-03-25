727ab415a3265a2f03bae531b8e36b88b3ed1d3e
Nimrod Dickler <nimrodd@traiana.com>
2017-03-25 12:56:45 +0300

    imageSharpning is working

--- Q1/imageSharpening.m
+++ Q1/imageSharpening.m
@@ -1,11 +1,14 @@
 function [sharpImage] = imageSharpening(image,sharpRate)
-% scanning the image
+
     %Build The Matrix
     A = ([1 1 1; 1 1 1; 1 1 1])/9;
+    
     %Return the central convolution
     Smooth = conv2(image,double(A), 'same');
+    
     %Return the detailed image (according to the furmula)
     Detail = uint8(image) - uint8(Smooth);
+    
     %according to the formula as well (sharpened = original +(scalar*detailed))
     sharpImage = image + (sharpRate * Detail);
 end
