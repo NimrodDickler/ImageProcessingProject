eb097e2b0bc17d7e4c3362d419d8a9b03f6b6127
Nimrod Dickler <nimrodd@traiana.com>
2017-03-26 22:26:56 +0300

    after hough tests

--- Q2/hough_transform.py
+++ Q2/hough_transform.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy import misc
 
+
 def hough_line(img, angle_step=1):
-  """
+    """
   Hough transform for lines
 
   Input:
@@ -16,57 +17,64 @@ def hough_line(img, angle_step=1):
   rhos - array of rho values. Max size is 2 times the diagonal
          distance of the input image.
   """
-  # Rho and Theta ranges
-  thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
-  width, height= img.shape
-  diag_len = int(np.ceil(np.sqrt(width * width + height * height)))
-  rhos = np.linspace(-diag_len, diag_len, diag_len * 2.0)
+    # Rho and Theta ranges
+    thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
+    width, height = img.shape
+    diag_len = int(np.ceil(np.sqrt(width * width + height * height)))
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2.0)
 
-  # Cache some resuable values
-  cos_t = np.cos(thetas)
-  sin_t = np.sin(thetas)
-  num_thetas = len(thetas)
+    # Cache some resuable values
+    cos_t = np.cos(thetas)
+    sin_t = np.sin(thetas)
+    num_thetas = len(thetas)
 
-  # Hough accumulator array of theta vs rho
-  accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
-  y_idxs, x_idxs = np.nonzero(img)  # (row, col) indexes to edges
+    # Hough accumulator array of theta vs rho
+    accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
+    y_idxs, x_idxs = np.nonzero(img)  # (row, col) indexes to edges
 
-  # Vote in the hough accumulator
-  for i in range(len(x_idxs)):
-    x = x_idxs[i]
-    y = y_idxs[i]
+    # Vote in the hough accumulator
+    for i in range(len(x_idxs)):
+        x = x_idxs[i]
+        y = y_idxs[i]
 
-    for t_idx in range(num_thetas):
-      # Calculate rho. diag_len is added for a positive index
-      rho = int(round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len)
-      accumulator[rho, t_idx] += 1
+        for t_idx in range(num_thetas):
+            # Calculate rho. diag_len is added for a positive index
+            rho = int(round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len)
+            accumulator[rho, t_idx] += 1
+    print "Done Accumelator"
+    return accumulator, thetas, rhos
 
-  return accumulator, thetas, rhos
 
 def show_hough_line(img, accumulator):
-  import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-  fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+    fig, ax = plt.subplots(1, 2, figsize=(10, 10))
 
-  ax[0].imshow(img, cmap=plt.cm.gray)
-  ax[0].set_title('Input image')
-  ax[0].axis('image')
+    ax[0].imshow(img, cmap=plt.cm.gray)
+    ax[0].set_title('Input image')
+    ax[0].axis('image')
 
-  ax[1].imshow(
-    accumulator, cmap='jet',
-    extent=[np.rad2deg(thetas[-1]), np.rad2deg(thetas[0]), rhos[-1], rhos[0]])
-  ax[1].set_aspect('equal', adjustable='box')
-  ax[1].set_title('Hough transform')
-  ax[1].set_xlabel('Angles (degrees)')
-  ax[1].set_ylabel('Distance (pixels)')
-  ax[1].axis('image')
+    ax[1].imshow(
+        accumulator, cmap='jet',
+        extent=[np.rad2deg(thetas[-1]), np.rad2deg(thetas[0]), rhos[-1], rhos[0]])
+    ax[1].set_aspect('equal', adjustable='box')
+    ax[1].set_title('Hough transform')
+    ax[1].set_xlabel('Angles (degrees)')
+    ax[1].set_ylabel('Distance (pixels)')
+    ax[1].axis('image')
 
-  #plt.axis('off')
-  plt.savefig('imgs/output.png', bbox_inches='tight')
-  plt.show()
+    # plt.axis('off')
+    plt.savefig('imgs/output.png', bbox_inches='tight')
+    plt.show()
+
 
 if __name__ == '__main__':
-  imgpath = 'imgs/binary_crosses.png'
-  img = misc.imread(imgpath,flatten=True)
-  accumulator, thetas, rhos = hough_line(img)
-  show_hough_line(img, accumulator)
+    imgpath = 'imgs/binary_crosses.png'
+    img = misc.imread(imgpath, flatten=True)
+    accumulator, thetas, rhos = hough_line(img)
+    print "After Lines Accumelator"
+    show_hough_line(img, accumulator)
+    print "After Show Accumelator"
+    print thetas
+    print rhos
+    print accumulator
--- Q2A/HoughTransform.py
+++ Q2A/HoughTransform.py
@@ -1,5 +1,52 @@
 import cv2
 import numpy as np
+from scipy import misc
+
+def hough_line(img, angle_step=1):
+    """
+  Hough transform for lines
+
+  Input:
+  img - 2D binary image with nonzeros representing edges
+  angle_step - Spacing between angles to use every n-th angle
+    between -90 and 90 degrees. Default step is 1.
+
+  Returns:
+  accumulator - 2D array of the hough transform accumulator
+  theta - array of angles used in computation, in radians.
+  rhos - array of rho values. Max size is 2 times the diagonal
+         distance of the input image.
+  """
+    # Rho and Theta ranges
+    thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
+    width, height = img.shape
+    diag_len = int(np.ceil(np.sqrt(width * width + height * height)))
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2.0)
+
+    # Cache some resuable values
+    cos_t = np.cos(thetas)
+    sin_t = np.sin(thetas)
+    num_thetas = len(thetas)
+
+    # Hough accumulator array of theta vs rho
+    accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint64)
+    y_idxs, x_idxs = np.nonzero(img)  # (row, col) indexes to edges
+
+    # Vote in the hough accumulator
+    for i in range(len(x_idxs)):
+        x = x_idxs[i]
+        y = y_idxs[i]
+
+        for t_idx in range(num_thetas):
+            # Calculate rho. diag_len is added for a positive index
+            rho = int(round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len)
+            accumulator[rho, t_idx] += 1
+    print "Done Accumelator"
+    return accumulator, thetas, rhos
+
+#imgpath = "D:\Git\Private\ImageProcessingProject\imgs\CheeseBoard.jpg"
+#img = misc.imread(imgpath, flatten=True)
+#accumulator, thetas, rhos = hough_line(img)
 
 
 img = cv2.imread('D:\Git\Private\ImageProcessingProject\imgs\CheeseBoard.jpg')
@@ -8,7 +55,9 @@ edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,200)
 
-for rho,theta in lines[0]:
+print "nimrod"
+
+for rho,theta in lines:
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a*rho
